2340e1c52c6a937b943b1ed08f517e9aca26b805
Franco <fmacke10@gmail.com>
2026-03-14 12:40:36 -0300

    djortcuts with overseet

--- lua/plugins/core/djortcuts.lua
+++ lua/plugins/core/djortcuts.lua
@@ -1,33 +1,38 @@
 return {
   {
-    "franmacke/djortcuts.nvim",
+    -- "franmacke/djortcuts.nvim",
+    dir = "/home/franco/Documents/Codes/djortcuts.nvim",
+    dependencies = { "stevearc/overseer.nvim" },
     lazy = true,
     priority = 1000,
     keys = {
       -- Core Django commands (<leader>j)
-      { "<leader>jr", "<cmd>DjangoRun<cr>", desc = "Django: Run Server" },
-      { "<leader>js", "<cmd>DjangoShell<cr>", desc = "Django: Shell" },
-      { "<leader>jt", "<cmd>DjangoTest<cr>", desc = "Django: Test" },
-      { "<leader>ji", "<cmd>DjangoInit<cr>", desc = "Django: Init Config" },
+      { "<leader>jr",  "<cmd>DjangoRun<cr>",               desc = "Django: Run Server" },
+      { "<leader>js",  "<cmd>DjangoShell<cr>",             desc = "Django: Shell" },
+      { "<leader>jt",  "<cmd>DjangoTest<cr>",              desc = "Django: Test" },
+      { "<leader>ji",  "<cmd>DjangoInit<cr>",              desc = "Django: Init Config" },
 
       -- Migrations (<leader>jm*)
-      { "<leader>jmm", "<cmd>DjangoMakemigrations<cr>", desc = "Django: Make Migrations" },
-      { "<leader>jma", "<cmd>DjangoMigrate<cr>", desc = "Django: Apply Migrations" },
+      { "<leader>jmm", "<cmd>DjangoMakemigrations<cr>",    desc = "Django: Make Migrations" },
+      { "<leader>jma", "<cmd>DjangoMigrate<cr>",           desc = "Django: Apply Migrations" },
 
       -- App/Project management (<leader>ja*, <leader>jp*)
-      { "<leader>ja", "<cmd>DjangoStartapp<cr>", desc = "Django: Start App" },
-      { "<leader>jp", "<cmd>DjangoStartproject<cr>", desc = "Django: Start Project" },
+      { "<leader>ja",  "<cmd>DjangoStartapp<cr>",          desc = "Django: Start App" },
+      { "<leader>jp",  "<cmd>DjangoStartproject<cr>",      desc = "Django: Start Project" },
 
       -- Static files & checks (<leader>jc*, <leader>jk*)
-      { "<leader>jc", "<cmd>DjangoCollectstatic<cr>", desc = "Django: Collect Static" },
-      { "<leader>jk", "<cmd>DjangoCheck<cr>", desc = "Django: Check Project" },
+      { "<leader>jc",  "<cmd>DjangoCollectstatic<cr>",     desc = "Django: Collect Static" },
+      { "<leader>jk",  "<cmd>DjangoCheck<cr>",             desc = "Django: Check Project" },
 
       -- Management commands (<leader>jj)
-      { "<leader>jj", "<cmd>DjangoManagementCommand<cr>", desc = "Django: Management Command" },
+      { "<leader>jj",  "<cmd>DjangoManagementCommand<cr>", desc = "Django: Management Command" },
 
       -- i18n (internationalization) (<leader>jx*)
-      { "<leader>jxm", "<cmd>DjangoMakemessages<cr>", desc = "Django i18n: Make Messages" },
-      { "<leader>jxc", "<cmd>DjangoCompilemessages<cr>", desc = "Django i18n: Compile Messages" },
+      { "<leader>jxm", "<cmd>DjangoMakemessages<cr>",      desc = "Django i18n: Make Messages" },
+      { "<leader>jxc", "<cmd>DjangoCompilemessages<cr>",   desc = "Django i18n: Compile Messages" },
+
+      -- Overseer
+      { "<leader>jo",  "<cmd>OverseerToggle<cr>",          desc = "Django: Overseer command panel" }
     },
     config = function()
       local django_automation = require("djortcuts")
---
+++ lua/plugins/core/gitsigns.lua
@@ -0,0 +1,17 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
+    },
+    current_line_blame_opts = {
+      delay = 200,
+    },
+  }
+}
